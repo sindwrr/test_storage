aa0de4b98af520959e9cbc7f42eecfa405b595bc
sindwrr <stepanpilipcenko@gmail.com>
2026-05-05 22:59:24 +0300

    Refactor health service

--- internal/health/interface.go
+++ internal/health/interface.go
@@ -1,26 +1,8 @@
 package health
 
-import (
-	"context"
-	"database/sql"
-	"fmt"
-)
+import "context"
 
-type healthService struct {
-	db *sql.DB
-}
-
-func NewService(db *sql.DB) HealthService {
-	return &healthService{db: db}
-}
-
-func (s *healthService) Alive(ctx context.Context) error {
-	return nil
-}
-
-func (s *healthService) Ready(ctx context.Context) error {
-	if s.db == nil {
-		return fmt.Errorf("DB not configured!")
-	}
-	return s.db.PingContext(ctx)
+type HealthService interface {
+	Alive(ctx context.Context) error
+	Ready(ctx context.Context) error
 }
--- internal/health/service.go
+++ internal/health/service.go
@@ -1,8 +1,26 @@
 package health
 
-import "context"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
 
-type HealthService interface {
-	Alive(ctx context.Context) error
-	Ready(ctx context.Context) error
+type healthService struct {
+	db *sql.DB
+}
+
+func NewService(db *sql.DB) HealthService {
+	return &healthService{db: db}
+}
+
+func (s *healthService) Alive(ctx context.Context) error {
+	return nil
+}
+
+func (s *healthService) Ready(ctx context.Context) error {
+	if s.db == nil {
+		return fmt.Errorf("DB not configured!")
+	}
+	return s.db.PingContext(ctx)
 }
